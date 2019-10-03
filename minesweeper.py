4a6a21705b50d4e3a17fc989030ca67619f6092e
Gabriel Breeding <gbreeding@debian14.CIS.COM>
2019-10-03 08:40:41 -0500

    modified:   minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+#GabrielBreeding
 
-# [[a, s, d] row 1?
-#  [f, g, h] row 2?
+'''a program where the user can interact with a list to reveal the lists true contents'''
+
+
+#---------------imports------------------
+import sys
+
 
 #lists the first, unrevealed field.
-field = [['1', '2', '3'],
-         ['4', '5', '6'],
-         ['7', '8', '9'],
+field = [['1' , '2' , '3'],
+         ['4' , '5' , '6'],
+         ['7' , '8' , '9'],
          ['10', '11', '12'],
          ['13', '14', '15']]
 
@@ -16,69 +21,51 @@ revealed = [["a", 'b', 'c'],
             ['g', 'h', 'i'],
             ['j', 'k', 'l'],
             ['m', 'n', 'o']]
-
-#prints the field for user to see.
-print("field =", field)
-
 #makes newlist variable for the reason of being manipulated.
 newlist = field
 
+# holds the current field
 def hold():
-        print("1.", newlist[0])
-        print("1.", newlist[0])
-        print("2.", newlist[1])
-        print("3.", newlist[2])
-        print("4.", newlist[3])
-        print("5.", newlist[4])
+        print("            columns       ")
+        print("    ")
+        print("         1.    2.    3.   ")
+        print("    1.", newlist[0][0], "  |  ", newlist[0][1], "  |  ", newlist[0][2])
+        print("        ----------------")
+        print(" r  2.", newlist[1])
+        print(" o      ----------------")
+        print(" w  3.", newlist[2])
+        print(" s      ----------------")
+        print("    4.", newlist[3])
+        print("        ----------------")
+        print("    5.", newlist[4])
         main()
-
 
 #makes the main program code for interacting with newlist.
 def main():
+#user inputs which row and collumn to reveal
     print("        ")
     print("        ")
-    reveal = input("select which row to reveal: ")
-    reveal = float(reveal)
+    revrow = input("Please select which row to reveal: ")
+    if revrow == "exit":
+        print("Goodbye!")
+        sys.exit()
+    print("        ")
+    print("        ")
+    revcol = input("Please select which collumn to reveal: ")
+    if revcol == "exit":
+        print("Goodbye!")
+        sys.exit()
+    revcol = int(revcol)-1
+    revrow = int(revrow)-1
 #if everything is revealed, activate this.
     if newlist == revealed:
         print("all is revealed")
-#if the input is 1, check if the first collumn is already revealed and then if not, reveal it.
-    elif reveal == 1:
-        if newlist[0] == revealed[0]:
-            print("already revealed")
-        else:
-            newlist[0] = revealed[0]
-            hold()
-#if the input is 2, check if the second collumn is already revealed and if not, reveal it.
-    elif reveal == 2:
-        if newlist[1] == revealed[1]:
-            print("already revealed")
-        else:
-            newlist[1] = revealed[1]
-            hold()
-
-    elif reveal == 3:
-        if newlist[2] == revealed[2]:
-            print("already revealed")
-        else:
-            newlist[2] = revealed[2]
-            hold()
-
-    elif reveal == 4:
-        if newlist[3] == revealed[3]:
-            print("already revealed")
-        else:
-            newlist[3] = revealed[3]
-            hold()
-
-    elif reveal == 5:
-        if newlist[4] == revealed[4]:
-            print("already revealed")
-        else:
-            newlist[4] = revealed[4]
-            hold()
-
-
-
+    elif newlist[revrow][revcol] == revealed[revrow][revcol]:
+        print("Sorry! That is already revealed!")
+        main()
+    else:
+        newlist[revrow][revcol] = revealed[revrow][revcol]
+        hold()
+hold()
 #loops the program
 main()
